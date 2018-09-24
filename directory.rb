a5899c9018dc779790144be90708ccd0a1061ddd
Imogen Misso <imogenmisso@hotmail.co.uk>
2018-09-24 20:32:08 +0100

    added print by cohort method

--- directory.rb
+++ directory.rb
@@ -1,3 +1,24 @@
+def interactive_menu
+  students = []
+  loop do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit"
+    selection = gets.chomp
+    case selection
+      when "1"
+        students = input_students
+      when "2"
+        print_header
+        print(students)
+        print_footer(students)
+      when "9"
+        exit
+      else
+        puts "I don't know what you meant, try again"
+    end
+  end
+end
 def input_students
   puts "Please enter the names of the students, followed by some personal information"
   puts "To finish, just hit return twice"
@@ -16,7 +37,7 @@ def input_students
       hobby = gets.strip
     puts "Country of birth"
       country = gets.strip
-    puts "What is their height?"
+    puts "What is their height in cm?"
       height = gets.strip.to_s
     students << {name: name, cohort: cohort, hobby: hobby, country: country, height: height}
     puts "Please enter another name"
@@ -29,7 +50,16 @@ def print_header
   puts "The students of Villains Academy".center(80)
   puts "_____________".center(80)
 end
+
 def print(students)
+  students.each_with_index do |student, index|
+    if student[:name].length < 12
+      puts ("#{index + 1}. #{student[:name]}, #{student[:cohort]} cohort, likes #{student[:hobby]}, born in #{student[:country]}, is #{student[:height]}cm tall").center(80)
+    end
+  end
+end
+
+def print_by_cohort(students)
   sorted_by_cohort = {}
   students.each do |students_under_consideration|
     cohort = students_under_consideration[:cohort]
@@ -40,10 +70,14 @@ def print(students)
       sorted_by_cohort[cohort].push(name)
     end
   end
-  sorted_by_cohort.each do |key, value|
-    puts (key.to_s + ": " + value.to_s).center(80)
-  end
+  sorted_by_cohort.each do |cohort, students|
+    puts "#{cohort} cohort:".center(80)
+      students.each_with_index do |name, index|
+        puts "#{index + 1}. #{name}".center(80)
+      end
+    end
 end
+
 def print_footer(students)
   if students.count == 1
     puts "Overall, we have #{students.count} great student".center(80)
@@ -51,8 +85,7 @@ def print_footer(students)
     puts "Overall, we have #{students.count} great students".center(80)
   end
 end
-#nothing happens until we call the methods
 students = input_students
 print_header
-print(students)
+print_by_cohort(students)
 print_footer(students)
